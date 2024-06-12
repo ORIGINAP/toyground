17de3698467a46ff9b8a5e5e016d4e74283ce536
ORIGINAP <162403242+ORIGINAP@users.noreply.github.com>
2024-06-12 02:50:36 +0000

    new

---
+++ clang_/dfs.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stack.h>
+
+/*
+들어온 컴퓨터 대수 만큼 테이블 생성
+
+*/
+int main(){
+    
+}
---
+++ clang_/stack.h
@@ -0,0 +1,7 @@
+#ifndef STACK_H
+#define STACK_H
+
+int     IsEmpty();
+int     IsFull();
+void    push(int val);
+int     pop();
