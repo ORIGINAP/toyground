16c92157961127df12b9d8f5e4da64da22f080ef
ORIGINAP <162403242+ORIGINAP@users.noreply.github.com>
2024-06-12 12:17:00 +0000

    new

--- clang_/dfs.c
+++ clang_/dfs.c
@@ -1,10 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stack.h>
+#include<stdbool.h>
+#define MAX_SIZE 100
 
-/*
-들어온 컴퓨터 대수 만큼 테이블 생성 -> ok
-*/
+int stack[MAX_SIZE];
+int top = -1;
+
+int IsEmpty(){
+    if(top<0)
+        return true;
+    else
+        return false;
+}
+int IsFull(){
+    if(top == MAX_SIZE-1)
+        return true;
+    else
+        return false;
+}
+void push(int val){
+    if(IsFull()==true)
+        printf("Stack is full");
+    else
+        stack[++top] = val;
+}
+int pop(){
+    if(IsEmpty()==true)
+        printf("Stack is Empty");
+    else
+        return stack[top--];
+    return -1;
+}
+
+void DFS(int start_vertex,int** graph,int* visited, int max_vertex){
+    int connected_net=0;
+    //시작정점을 방문
+    push(start_vertex);
+    visited[start_vertex] = true;
+    while(IsEmpty()==false){
+        int ver = pop();
+        for(int i = 0; i<max_vertex; i++){
+            //인접정점 마다 네트워크 카운트
+            if(graph[ver][i]==1 && visited[i] == false){
+                push(i);
+                visited[i] = true;
+                connected_net++;
+            }
+        }
+    }
+    printf("%d\n",connected_net);
+}
 
 int main(){
     int     com_max;
@@ -19,19 +64,22 @@ int main(){
     visited = (int*)malloc(sizeof(int*)*com_max);
     //이중포인터로 행렬 생성.
     graph = (int**)malloc(sizeof(int**)*com_max);
-    *graph = (int*)malloc(sizeof(int*)*com_max);
+    for(int i=0; i<com_max; i++)
+        graph[i] = (int*)malloc(sizeof(int*)*com_max);
     //컴퓨터 연결쌍을 입력받는다.
     scanf("%d",&input_size);
     p1 = (int*)malloc(sizeof(int*)*input_size);
     p2 = (int*)malloc(sizeof(int*)*input_size);
     for(int i = 0; i < input_size; i++){
         scanf("%d %d",(p1+i),(p2+i));
-    }   
-}
-
-int DFS(int start, int** graph, int size){
-    그래프의 첫번째 요소부터 탐색을 시작.
-    행렬을 탐색할때, 방문한 노드를 스택에 넣는다. 그리고 인접노드중 방문하지 않았고, 정렬된 것중 우선되는 노드에 방문한다.
-    스택에 다시 넣는다. 만일 이미 방문한 노드라면 스킵하고 넣는다. 넣을게 없다면 그대로 pop을 한다.
-    위를 반복하면 결국 top이 -1이 될 것.
+        graph[*(p1+i)-1][*(p2+i)-1]=1;
+        graph[*(p2+i)-1][*(p1+i)-1]=1;
+    }
+    for(int i=0;i<com_max;i++){
+        for(int j=0; j<com_max; j++){
+            printf("[%d]",graph[i][j]);
+        }
+        printf("\n");
+    }
+    DFS(0,graph,visited,com_max);
 }
