6b8cf93393db422f674501231a646e97146b4446
ORIGINAP <162403242+ORIGINAP@users.noreply.github.com>
2024-06-12 04:57:24 +0000

    new

--- clang_/dfs.c
+++ clang_/dfs.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stack.h>
+//#include<stack.h>
 
 /*
 들어온 컴퓨터 대수 만큼 테이블 생성
 
 */
 int main(){
+    int     com_max;
+    int     input_size=0;
+    int*    p1;
+    int*    p2;
+
+    scanf("%d",&com_max);
+    scanf("%d",&input_size);
+    p1 = (int*)malloc(sizeof(int*)*input_size);
+    p2 = (int*)malloc(sizeof(int*)*input_size);
+    for(int i = 0; i < input_size; i++){
+        scanf("%d %d",(p1+i),(p2+i));
+    }
     
+    for(int i = 0; i < input_size; i++){
+        printf("%d %d\n",*(p1+i),*(p2+i));
+    }
 }
