38e41a4032223bd080b1de675c90b4c83cefabe3
ORIGINAP <162403242+ORIGINAP@users.noreply.github.com>
2025-03-14 08:33:39 +0000

    0314

--- bj/14888.py
+++ bj/14888.py
@@ -1,18 +1,21 @@
-opr_lst=["+","-","*","/"]
-def define_opr(pre,post):
-    pre_fix = pre.copy()
-    post_cp = post.copy()
-    if(len(post)>0):
-        for i in post:
-            pre_fix.append(post.pop(i))
-            define_opr(pre_fix, post)
+num_set=[1,2,3,4,5]
+opr_lst=["+","/","-","*"]
+wordset = ['','','','']
+depth = 0
+result = []
+opr_len = len(opr_lst)
+
+def select_opr(opr_lst,wordset,depth,result):
+    if(depth<opr_len):
+        for i in opr_lst:
+            opr_lst_cp=opr_lst.copy()
+            wordset[depth] = i
+            opr_lst_cp.remove(i)
+            select_opr(opr_lst_cp,wordset,depth+1,result)
     else:
-        print(pre_fix)
-
-for i in range(3,-1,-1):
-    post_opr = opr_lst.copy()
-    post_opr.remove(opr_lst[i])
-    pre_fix = [opr_lst[i]]
-    define_opr(pre_fix,post_opr)
-
-print("hello")
+        result += wordset
+        
+select_opr(opr_lst,wordset,depth,result)
+result = [result[i:i+4] for i in range(0,len(result),4)]
+print(result)
+print(len(result))
